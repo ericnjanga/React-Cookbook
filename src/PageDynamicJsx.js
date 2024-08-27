f2595a8c836c35ce9a2d2b55b574449947de80f2
Eric Njanga <eric.njanga@gmail.com>
2024-08-27 10:14:07 -0400

    Adding custom spacing

--- src/PageDynamicJsx.js
+++ src/PageDynamicJsx.js
@@ -4,6 +4,11 @@ import LiveOrder from './LiveOrders';
 
 const PageDynamicJsx = () => {
 
+    const spacinfStyle = {
+        // using string interpolation to set the margin
+        marginBottom: `3.5rem`,
+    };
+
     return (
         <>
             <div className="row">
@@ -16,14 +21,24 @@ const PageDynamicJsx = () => {
                     To solve this problem, we will use <pre><a href="https://react.dev/reference/react/Children" target="_blank">React.Children()</a></pre> map function to loop through 
                     every child of component, and <pre><a href="https://react.dev/reference/react/cloneElement" target="_blank">React.cloneElement</a></pre> to return a new copy of the 
                     element to which we will apply the new styles.</p>
-                <p></p>
-                <p></p>
-                <p></p>
             </div>
+
+            <div style={spacinfStyle}></div>
             
             <div className="row">
                 <h3>Code output</h3>
                 <LiveOrder />
+            </div>
+
+            <div style={spacinfStyle}></div>
+            
+            <div className="row">
+                <div className="col">
+                    <div className="alert alert-info" role="alert">
+                        <h5>Code sample</h5>
+                        <p><a href="https://github.com/ericnjanga/React-Cookbook/blob/main/src/LiveOrders.js" target="_blank">See the code on GitHUb</a></p>
+                    </div>
+                </div>
             </div>
         </>
     );
